65b1f3a58a3266c7ae2de957e62782cc1aae4595
Diego de la Fuente <diegodelafuentealonso@gmail.com>
2022-10-05 23:42:37 +0200

    Muestra apellidos años y comida favorita

--- Prueba.java
+++ Prueba.java
@@ -2,6 +2,7 @@ public class Prueba {
     public static void main(String args[]) {
         System.out.println("de la Fuente Alonso");
         System.out.println("18 años; 2003");
+        System.out.println("mi comida favorita es el solomillo de cerdo");
 
     }
 }
